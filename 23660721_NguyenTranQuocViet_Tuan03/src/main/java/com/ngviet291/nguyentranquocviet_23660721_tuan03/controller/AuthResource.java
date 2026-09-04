6b39d4f6c459a383e83939dd9134f8463ef9ea38
H82M19\Student <Student>
2026-09-04 08:57:41 +0700

    fix Tuan 3

--- 23660721_NguyenTranQuocViet_Tuan03/src/main/java/com/ngviet291/nguyentranquocviet_23660721_tuan03/controller/AuthResource.java
+++ 23660721_NguyenTranQuocViet_Tuan03/src/main/java/com/ngviet291/nguyentranquocviet_23660721_tuan03/controller/AuthResource.java
@@ -1,5 +1,7 @@
 package com.ngviet291.nguyentranquocviet_23660721_tuan03.controller;
 
+import com.ngviet291.nguyentranquocviet_23660721_tuan03.service.AuthenticationService;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -7,10 +9,13 @@ import jakarta.ws.rs.Path;
 @Named
 @Path("/auth")
 public class AuthResource{
+    @Inject
+    private AuthenticationService authenticationService;
     @POST
     @Path("/login")
     public String login(String username, String password){
-        if("student".equals(username)&&"123456".equals(password)){
+        boolean isAuthenticated = authenticationService.authentication(username, password);
+        if(isAuthenticated){
             return "Login successful";
         }else{
             return "Login failed";
